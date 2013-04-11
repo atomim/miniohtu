dc3e3c8b15306bcab4756b09f89ba0a08d6cad7e
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-04-11 12:18:50 +0300

    playing around with jbibtex

--- src/main/java/ohtu/miniohtu/App.java
+++ src/main/java/ohtu/miniohtu/App.java
@@ -1,13 +1,50 @@
 package ohtu.miniohtu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.util.Collection;
+import java.util.List;
+import org.jbibtex.*;
+
+public class App {
+
+    static public void main(String... args) throws Exception {
+        Tools tools = new Tools();
+        File input = new File("sigproc.bib");
+
+        BibTeXDatabase database = tools.parseBibTeX(input);
+
+        Key key = new Key("title");
+
+        Collection<BibTeXEntry> entries = (database.getEntries()).values();
+        for (BibTeXEntry entry : entries) {
+            Value value = entry.getField(key);
+
+            // The field is not defined
+            if (value == null) {
+                continue;
+            }
+
+            try {
+                String latexString = value.toUserString();
+               // System.out.println(latexString);
+
+                List<LaTeXObject> objects = tools.parseLaTeX(latexString);
+
+                String plainTextString = tools.printLaTeX(objects);
+                System.out.println(plainTextString);
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            }
+
+            System.out.println();
+        }
     }
+    
 }
