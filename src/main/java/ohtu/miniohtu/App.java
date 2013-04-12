c6be4c87c83d85f93e3b811322e45c1cffb3970c
Olavi Lintumäki <atomimalli@gmail.com>
2013-04-12 04:39:42 +0300

    Removed hello world

--- src/main/java/ohtu/miniohtu/App.java
+++ src/main/java/ohtu/miniohtu/App.java
@@ -16,45 +16,6 @@ import org.jbibtex.*;
 public class App {
 
     static public void main(String... args) throws Exception {
-        Tools tools = new Tools();
-        File input = new File("sigproc.bib");
 
-        BibTeXDatabase database = tools.parseBibTeX(input);
-
-        Key key = new Key("title");
-
-        Collection<BibTeXEntry> entries = (database.getEntries()).values();
-        for (BibTeXEntry entry : entries) {
-            Value value = entry.getField(key);
-
-            // The field is not defined
-            if (value == null) {
-                continue;
-            }
-
-            try {
-                String latexString = value.toUserString();
-               // System.out.println(latexString);
-
-                List<LaTeXObject> objects = tools.parseLaTeX(latexString);
-
-                String plainTextString = tools.printLaTeX(objects);
-                System.out.println(plainTextString);
-            } catch (Exception e) {
-                e.printStackTrace(System.out);
-            }
-
-            System.out.println();
-        }
-        BibTeXFormatter bf = new BibTeXFormatter();
-        BibTeXDatabase db = new BibTeXDatabase();
-        BibTeXEntry bs = new BibTeXEntry(new Key("book"), new Key("666BOOK"));
-        bs.addField(BibTeXEntry.KEY_AUTHOR, new KeyValue("jarno kalmari"));
-        bs.addField(BibTeXEntry.KEY_BOOKTITLE, new KeyValue("jarno afganistanissa"));
-        
-        db.addObject(bs);
-        bf.format(db, new PrintWriter(System.out));
     }
-
-    
 }
