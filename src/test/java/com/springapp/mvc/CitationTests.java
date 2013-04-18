966aa07f060936f4e9addc611eb993bc0fe982e5
Olavi Lintumäki <atomimalli@gmail.com>
2013-04-19 01:39:52 +0300

    fix jenkins build by downgrading ebeans

--- src/test/java/com/springapp/mvc/CitationTests.java
+++ src/test/java/com/springapp/mvc/CitationTests.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 //@RunWith(SpringJUnit4ClassRunner.class)
-//@WebAppConfiguration
+@WebAppConfiguration
 //@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 public class CitationTests {
 
@@ -28,79 +28,10 @@ public class CitationTests {
     public void setup() {
         
     }
-/*
-    @Test
-    public void booktitleWorks() {
-        citation.setBooktitle("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getBooktitle().toString());
-    }
-
-    @Test
-    public void YearWorks() {
-        citation.setYear("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getYear().toString());
-    }
-
-    @Test
-    public void AuthorWorks() {
-        citation.setAuthor("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getAuthor().toString());
-    }
-
-    @Test
-    public void PublisherWorks() {
-        citation.setPublisher("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getPublisher().toString());
-    }
-
-    @Test
-    public void PagesWorks() {
-        citation.setPages("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getPages().toString());
-    }
-
-    @Test
-    public void AddressWorks() {
-        citation.setAddress("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getAddress().toString());
-    }
-
-    @Test
-    public void VolumeWorks() {
-        citation.setVolume("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getVolume().toString());
-    }
-
-    @Test
-    public void NumberWorks() {
-        citation.setNumber("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getNumber().toString());
-    }
-
-    @Test
-    public void JournalWorks() {
-        citation.setJournal("asdfgasgasdf");
-        assertEquals("asdfgasgasdf", citation.getJournal().toString());
-    }
 
     @Test
     public void toStringWorks() {
-        citation.setBooktitle("asdf");
-        assertEquals("asdf", citation.toString());
+        
     }
-    
-    @Test
-    public void bibtextInstanceIsValid() {
-        assertTrue(citation.getBibTeXEntry() != null);
-    }
-    
-    @Test
-    public void returnsFieldMapSuccessfully() {
-        citation.setAuthor("asdf");
-        citation.setBooktitle("Asdf");
-        assertEquals(2, citation.getFieldMap().size());
-    }
-=======
-  
-*/
+
 }
