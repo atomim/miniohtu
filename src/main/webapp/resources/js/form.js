e5b956ccc0127a4c3c736c9a32d18fcb33973440
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-04-25 00:47:24 +0300

    forms almost working

--- src/main/webapp/resources/js/form.js
+++ src/main/webapp/resources/js/form.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-    
-  $("#newForm").click(function(){
-   $("<input class='text' name='key' type='text'><input class='text' name='value' type='text'>").appendTo("#formSpace");
-  });
-  
-  $("#add").click(function() {
-      
-  });
-  
+var i = 0;
+function change() {
+    $("#key-" + i).val($("#value-" + i).val());
+    i++;
+}
+$(document).ready(function() {
+    $("#newForm").click(function() {
+        $("<input class='text' name='' type='text' id='key-" + i + "' onchange='this.name=this.value'><input class='text' type='text' id='value-" + i + "' onchange='change()'>").appendTo("#formSpace");
+    });
 });
-
