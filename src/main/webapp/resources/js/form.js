5cf9407ab29f220b451cf81e707158a6fd7d02a0
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-04-25 20:44:03 +0300

    form working(?)

--- src/main/webapp/resources/js/form.js
+++ src/main/webapp/resources/js/form.js
@@ -1,10 +1,22 @@
 var i = 0;
-function change() {
-    $("#key-" + i).val($("#value-" + i).val());
-    i++;
+
+function changeName(value) {
+    console.log("#input-" + (i-1));
+    console.log(value);
+    $("#input-" + (i-1))[0].setAttribute("name", value);
 }
+
+function changeValue(value) {
+    console.log("#input-" + (i-1));
+    console.log(value);
+    $("#input-" + (i-1))[0].setAttribute("value", value);
+}
+
 $(document).ready(function() {
     $("#newForm").click(function() {
-        $("<input class='text' name='' type='text' id='key-" + i + "' onchange='this.name=this.value'><input class='text' type='text' id='value-" + i + "' onchange='change()'>").appendTo("#formSpace");
+        $("<br><input class='text' type='text' placeholder='Type..' onkeyup='changeName(this.value)'>\n\
+           <input class='text' type='text' placeholder='Value..' onkeyup='changeValue(this.value)'>\n\
+           <input class='hidden' id='input-"+i+"' style=display:none>").appendTo("#formSpace");
+        i++;
     });
 });
